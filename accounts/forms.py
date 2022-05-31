4846b519e0cccd57a0a71cddce0a74d7a70b5d6d
drutkoowski <d.rutkowski2000@gmail.com>
2022-05-31 15:18:37 +0200

    creating templates and setting up login/signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,3 +13,20 @@ class UserForm(forms.ModelForm):
         self.fields["password"].widget.attrs["placeholder"] = "Password"
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class UserSignUpForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'username', 'email', 'phone_number', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserSignUpForm, self).__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs["placeholder"] = "First name"
+        self.fields["last_name"].widget.attrs["placeholder"] = "Last name"
+        self.fields["username"].widget.attrs["placeholder"] = "Username"
+        self.fields["email"].widget.attrs["placeholder"] = "Email"
+        self.fields["phone_number"].widget.attrs["placeholder"] = "Phone number"
+        self.fields["password"].widget.attrs["placeholder"] = "Password"
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import UserForm
+from .forms import UserForm, UserSignUpForm
 
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        print(username, password)
     form = UserForm()
     context = {
         "form": form,
     }
-    return render(request, "login.html", context=context)
+    return render(request, "accounts/login.html", context=context)
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        print(username, password)
+    form = UserSignUpForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context=context)
