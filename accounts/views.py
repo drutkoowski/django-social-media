8423c6f6a80ee7dbd5bf7c59f4af6771dd5b17a3
drutkoowski <d.rutkowski2000@gmail.com>
2022-06-13 12:31:37 +0200

    adding iexact to username search

--- accounts/views.py
+++ accounts/views.py
@@ -24,7 +24,7 @@ def login(request):
         if request.method == "POST":
             username = request.POST["username"]
             password = request.POST["password"]
-            user = auth.authenticate(username=username, password=password)
+            user = auth.authenticate(username__iexact=username, password=password)
             if user is not None:
                 auth.login(request, user)
                 messages.success(request, "You are now logged in")
