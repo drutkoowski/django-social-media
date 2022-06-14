5e3721bf85a9a682911f2cb2022aed611dea8795
drutkoowski <d.rutkowski2000@gmail.com>
2022-06-15 01:26:05 +0200

    adding s3 aws to serve static/media files for heroku

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -184,7 +184,6 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": 'max-age=86400'
 
