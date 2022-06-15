c77c53fe512272d48fed0ff4adf5eb51fbc38d65
drutkoowski <d.rutkowski2000@gmail.com>
2022-06-15 14:55:52 +0200

    adding s3 aws to serve static/media files for heroku

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -181,18 +181,20 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', cast=str)
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', cast=str)
 
 AWS_S3_FILE_OVERWRITE = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": 'max-age=86400'
 
 }
 AWS_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_QUERYSTRING_AUTH = False
 AWS_HEADERS = {
     "Access-Control-Allow-Origin": "*",
 }
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+STATICFILES_DIRS = [
+    'instaclone/static',
+]
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-
+DEFAULT_FILE_STORAGE = "instaclone.media_storages.MediaStorage"
